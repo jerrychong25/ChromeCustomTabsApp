8c8e61f5fbdabd9ec3d31872f5d6f0cc18adc2fa
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2019-04-16 11:06:15 +0800

    Added first commit
    -Display Chrome Custom Tabs once open the app

---
+++ app/src/main/java/com/example/chromecustomtabs/MainActivity.kt
@@ -0,0 +1,149 @@
+package com.example.chromecustomtabs
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var customTabHelper: CustomTabHelper = CustomTabHelper()
+
+    companion object {
+        private const val WEBSITE_URL = "http://www.google.com/"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val builder = CustomTabsIntent.Builder()
+
+        // modify toolbar color
+        builder.setToolbarColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
+
+        // add share button to overflow menu
+        builder.addDefaultShareMenuItem()
+
+        val anotherCustomTab = CustomTabsIntent.Builder().build()
+
+        val requestCode = 100
+        val intent = anotherCustomTab.intent
+        intent.setData(Uri.parse(WEBSITE_URL))
+
+        val pendingIntent = PendingIntent.getActivity(this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // add menu item to oveflow
+        builder.addMenuItem("Sample item", pendingIntent)
+
+        // menu item icon
+        // val bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)
+        // builder.setActionButton(bitmap, "Android", pendingIntent, true)
+
+        // modify back button icon
+        // builder.setCloseButtonIcon(bitmap)
+
+        builder.enableUrlBarHiding();
+
+        // hide website title
+        builder.setShowTitle(true)
+
+        // animation for enter and exit of tab
+        builder.setStartAnimations(this, android.R.anim.fade_in, android.R.anim.fade_out)
+        builder.setExitAnimations(this, android.R.anim.fade_in, android.R.anim.fade_out)
+
+        val customTabsIntent = builder.build()
+
+        // check is chrom available
+        val packageName = customTabHelper.getPackageNameToUse(this, WEBSITE_URL)
+
+        if (packageName == null) {
+            // if chrome not available open in web view
+            val intentOpenUri = Intent(this, WebViewActivity::class.java)
+            intentOpenUri.putExtra(WebViewActivity.EXTRA_URL, Uri.parse(WEBSITE_URL).toString())
+            startActivity(intentOpenUri)
+        } else {
+            customTabsIntent.intent.setPackage(packageName)
+            customTabsIntent.launchUrl(this, Uri.parse(WEBSITE_URL))
+        }
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
